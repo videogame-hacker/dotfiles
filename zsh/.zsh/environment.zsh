4e3796cb6998eb6b2512b49c4905da6edb96ee05
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-01-13 21:39:35 +0000

    Remove rubygems from PATH

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -1,8 +1,3 @@
-# Ruby gems in PATH
-if which ruby >/dev/null && which gem >/dev/null; then
-    PATH="$(ruby -rubygems -e 'puts Gem.user_dir')/bin:$PATH"
-fi
-
 # npm packages in PATH
 NPM_PACKAGES="${HOME}/.npm-packages"
 PATH="$NPM_PACKAGES/bin:$PATH"
