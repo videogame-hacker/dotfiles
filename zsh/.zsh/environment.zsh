7bb8b417adacdd546c82b179af167876a63c5dab
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-04-05 14:09:32 +0100

    Source NVM for environment

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -2,6 +2,12 @@
 NPM_PACKAGES="${HOME}/.npm-packages"
 PATH="$NPM_PACKAGES/bin:$PATH"
 
+# Source environment for nvm if installed
+if [ -f "/usr/share/nvm/init-nvm.sh" ]
+then
+    source /usr/share/nvm/init-nvm.sh
+fi
+
 # Editor and pager
 export EDITOR="vim"
 export VISUAL="less"
