e6e2f4ee459f483ef6d3cf0db05461dd6fe34223
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-04-06 13:53:14 +0100

    Lazy-source nvm to speed up shell startup time

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -5,7 +5,11 @@ PATH="$NPM_PACKAGES/bin:$PATH"
 # Source environment for nvm if installed
 if [ -f "/usr/share/nvm/init-nvm.sh" ]
 then
-    source /usr/share/nvm/init-nvm.sh
+    lazy_source () {
+        eval "$1 () { [ -f $2 ] && source $2 && $1 \$@ }"
+    }
+    
+    lazy_source nvm /usr/share/nvm/init-nvm.sh
 fi
 
 # Editor and pager
