a1784a05c212ac17b665e55b6433b1f3dd4017ac
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-06-02 14:13:10 +0100

    Add third monitor to hackery-desktop

--- bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
+++ bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
@@ -3,7 +3,10 @@
 # DVI-I-1 - ASUS VG248QE
 # DVI-D-0 - SyncMaster
 
-xrandr --output DVI-I-1 --mode 1920x1080 --rate 144
+xrandr --output DVI-I-1 --primary --mode 1920x1080 --rate 144 --pos 1280x0 \
+    --output DVI-D-0 --mode 1680x1050 --pos 3200x0 \
+    --output HDMI-0 --mode 1280x1024 --rate 75 --pos 0x0 \
 
-bspc monitor DVI-I-1 -d I II III IV V VI VII
-bspc monitor DVI-D-0 -d VIII IX X
+bspc monitor DVI-I-1 -d 1 2 3 4 5 6 7
+bspc monitor DVI-D-0 -d 8 9 10
+bspc monitor HDMI-0 -d 11
