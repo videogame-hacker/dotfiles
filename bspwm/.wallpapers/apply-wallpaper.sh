9c8b19a5673eef7d934b6daa94415d227e6fde8d
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-06-13 21:29:31 +0100

    Replace a wallpaper that was too brightly coloured

--- bspwm/.wallpapers/apply-wallpaper.sh
+++ bspwm/.wallpapers/apply-wallpaper.sh
@@ -2,14 +2,16 @@
 
 WALLPAPERS=(
     "https://images.unsplash.com/photo-1506348424954-b03ae28965ec"
-    "https://images.unsplash.com/photo-1441805983468-f5a1a9f985fb"
+    "https://images.unsplash.com/photo-1461884430365-0a87b6123549"
     "https://images.unsplash.com/photo-1523650126631-9fafa6ed33fc"
     "https://images.unsplash.com/photo-1502533609822-c877fe10f870"
 )
 
 function fetch() {
+    mkdir "$HOME/.wallpapers/images/"
+    
     for i in "${!WALLPAPERS[@]}"; do
-        wget -O "$HOME/.wallpapers/wallpaper-$i.jpg" "${WALLPAPERS[$i]}" &
+        wget -O "$HOME/.wallpapers/images/wallpaper-$i.jpg" "${WALLPAPERS[$i]}" &
     done
 
     wait $(jobs -p)
@@ -17,8 +19,8 @@ function fetch() {
 
 last=#WALLPAPERS[@]-1
 
-if [ ! -f "$HOME/.wallpapers/wallpaper-${last}.jpg" ]; then
+if [ ! -f "$HOME/.wallpapers/images/wallpaper-${last}.jpg" ]; then
     fetch
 fi
 
-feh --randomize --bg-fill "$HOME/.wallpapers/"
+feh --randomize --bg-fill "$HOME/.wallpapers/images"
