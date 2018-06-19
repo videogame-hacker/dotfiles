9e475db28d56af220c109f3a757595eea9438c05
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-06-19 22:15:32 +0100

    Update wallpapers

--- bspwm/.wallpapers/apply-wallpaper.sh
+++ bspwm/.wallpapers/apply-wallpaper.sh
@@ -5,14 +5,15 @@ WALLPAPERS=(
     "https://images.unsplash.com/photo-1461884430365-0a87b6123549"
     "https://images.unsplash.com/photo-1523650126631-9fafa6ed33fc"
     "https://images.unsplash.com/photo-1502533609822-c877fe10f870"
-    "https://images.unsplash.com/photo-1523988628690-c6661ec0e53d"
-    "https://images.unsplash.com/photo-1521794286957-348245b517a0"
     "https://images.unsplash.com/photo-1505526484636-f0a465cf6d19"
+    "https://images.unsplash.com/photo-1498036882173-b41c28a8ba34"
+    "https://images.unsplash.com/photo-1517176910664-8106c69b644a"
+    "https://images.unsplash.com/photo-1473711186548-79d2ee78cdec"
 )
 
 function fetch() {
     mkdir "$HOME/.wallpapers/images/"
-    
+
     for i in "${!WALLPAPERS[@]}"; do
         if [ ! -f "$HOME/.wallpapers/images/wallpaper-$i.jpg" ]; then
             wget -O "$HOME/.wallpapers/images/wallpaper-$i.jpg" "${WALLPAPERS[$i]}" &
