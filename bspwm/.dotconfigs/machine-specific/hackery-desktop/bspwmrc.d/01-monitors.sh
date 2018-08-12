aff88e2c1e60e5d2c631c032deaac1a6fc232370
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-08-12 13:50:55 +0100

    [bspwm] hackery-desktop monitors changed names for some reason

--- bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
+++ bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
@@ -4,7 +4,8 @@
 # DVI-D-0 - SyncMaster
 
 xrandr --output DVI-I-1 --primary --mode 1920x1080 --rate 144 --pos 1920x0 \
-    --output HDMI-1 --mode 1920x1080 --rate 75 --pos 0x0
+    --output HDMI-2 --mode 1920x1080 --rate 75 --pos 0x0
 
 bspc monitor DVI-I-1 -d 1 2 3 4 5 6 7
-bspc monitor HDMI-1 -d 8 9 10
+bspc monitor HDMI-2 -d 8 9 10
+
