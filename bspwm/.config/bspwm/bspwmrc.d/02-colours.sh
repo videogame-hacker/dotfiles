4737f04e5027d0c7da46b8cfe015e9ea3af69918
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-08-14 20:06:56 +0100

    [bspwm] Change borders back to monochrome & thin

--- bspwm/.config/bspwm/bspwmrc.d/02-colours.sh
+++ bspwm/.config/bspwm/bspwmrc.d/02-colours.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
-bspc config normal_border_color "#354757"
+bspc config normal_border_color "#000000"
 bspc config presel_feedback_color "#9aedfe" # Preselection is also made translucent with compton
-bspc config focused_border_color "#9aedfe"
+bspc config focused_border_color "#ffffff"
