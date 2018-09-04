e4d70768c3e0edcaa75a1d18fbc871b8632a0641
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-09-04 19:40:54 +0100

    [bspwm] Switch to ibus, change monitors on hackery-desktop, use thunar

--- bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
+++ bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
@@ -4,7 +4,7 @@
 # DVI-D-0 - SyncMaster
 
 xrandr --output DVI-I-1 --primary --mode 1920x1080 --rate 144 --pos 1920x0 \
-    --output HDMI-2 --mode 1920x1080 --rate 75 --pos 0x0
+    --output HDMI-0 --mode 1920x1080 --rate 75 --pos 0x0
 
 bspc monitor DVI-I-1 -d 1 2 3 4 5 6 7
-bspc monitor HDMI-2 -d 8 9 10
+bspc monitor HDMI-0 -d 8 9 10
--- polybar/.dotconfigs/polybar-scripts/get-uim-status-line.sh
+++ polybar/.dotconfigs/polybar-scripts/get-uim-status-line.sh
@@ -1,18 +1,1 @@
-#!/usr/bin/bash
-
-trim() {
-    local var="$*"
-    # remove leading whitespace characters
-    var="${var#"${var%%[![:space:]]*}"}"
-    # remove trailing whitespace characters
-    var="${var%"${var##*[![:space:]]}"}"   
-    echo -n "$var"
-}
-
-uim_status_dump=$(uim-status)
-
-active_im=$(echo "$uim_status_dump" | grep branch | cut -d $'\n' -f 1 | cut -d $'\t' -f 2)
-im_status=$(echo "$uim_status_dump" | grep branch | cut -d $'\t' -f 3 | tr $'\n' ' ' | cut -d ' ' -f 1 --complement)
-im_status=$(trim $im_status)
-
-echo "  $active_im ($im_status)"
+true
