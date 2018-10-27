7b037432a1a84030e8b542917e906ad8c8394015
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-10-27 21:26:37 +0100

    [alacritty, bspwm] Use alacritty as main
    ALSO: Use nouveau in hackery-desktop so tweak monitor profile

--- bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
+++ bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 
 # DVI-I-1 - ASUS VG248QE
-# DVI-D-0 - SyncMaster
+# HDMI-1 - Ilyama ProLite
 
 xrandr --output DVI-I-1 --primary --mode 1920x1080 --rate 144 --pos 1920x0 \
-    --output HDMI-0 --mode 1920x1080 --rate 75 --pos 0x0
+    --output HDMI-1 --mode 1920x1080 --rate 75 --pos 0x0
 
 bspc monitor DVI-I-1 -d 1 2 3 4 5 6 7
-bspc monitor HDMI-0 -d 8 9 10
+bspc monitor HDMI-1 -d 8 9 10
