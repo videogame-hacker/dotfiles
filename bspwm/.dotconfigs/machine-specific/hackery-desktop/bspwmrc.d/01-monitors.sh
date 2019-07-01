221b55f588e53adbd05ade92e4066ac5c39d3158
half cambodian hacker man <half-kh-hacker@hackery.site>
2019-07-01 16:16:18 +0100

    [bspwm] Update monitor settings for hackery-desktop

--- bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
+++ bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 
-# DVI-I-1 - ASUS VG248QE
+# DP-0 - ASUS VG248QE
 # HDMI-0 - Ilyama ProLite
 
-xrandr --output DVI-I-1 --primary --mode 1920x1080 --rate 144 --pos 1920x0 \
+xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --pos 1920x0 \
     --output HDMI-0 --mode 1920x1080 --rate 75 --pos 0x0
 
-bspc monitor DVI-I-1 -d 1 2 3 4 5 6 7
+bspc monitor DP-0 -d 1 2 3 4 5 6 7
 bspc monitor HDMI-0 -d 8 9 10
