f871eea1c4fc639ee389bca8b246cb3b80eae974
half cambodian hacker man <half-kh-hacker@hackery.site>
2019-07-14 19:39:24 +0100

    [hackery-desktop] Switch back to DVI
    I think my GPU is dying or something, DP had some really weird signal
    loss

--- bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
+++ bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
@@ -3,8 +3,8 @@
 # DP-0 - ASUS VG248QE
 # HDMI-0 - Ilyama ProLite
 
-xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --pos 1920x0 \
+xrandr --output DVI-D-0 --primary --mode 1920x1080 --rate 144 --pos 1920x0 \
     --output HDMI-0 --mode 1920x1080 --rate 75 --pos 0x0
 
-bspc monitor DP-0 -d 1 2 3 4 5 6 7
+bspc monitor DVI-D-0 -d 1 2 3 4 5 6 7
 bspc monitor HDMI-0 -d 8 9 10
