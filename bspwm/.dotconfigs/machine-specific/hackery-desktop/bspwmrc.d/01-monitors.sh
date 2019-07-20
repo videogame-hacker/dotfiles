4c29a153e10936f96c7479481ca45f07b44c64b9
half cambodian hacker man <half-kh-hacker@hackery.site>
2019-07-20 23:59:06 +0100

    Move to an XFCE-bspwm combination, new GPU for hackery-desktop

--- bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
+++ bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/sh
 
-# DP-0 - ASUS VG248QE
-# HDMI-0 - Ilyama ProLite
+MONITORS=( $(bspc query -M --names) )
 
-xrandr --output DVI-D-0 --primary --mode 1920x1080 --rate 144 --pos 1920x0 \
-    --output HDMI-0 --mode 1920x1080 --rate 75 --pos 0x0
-
-bspc monitor DVI-D-0 -d 1 2 3 4 5 6 7
-bspc monitor HDMI-0 -d 8 9 10
+bspc monitor ${MONITORS[2]} -d 1 2 3 4 5 6 7
+bspc monitor ${MONITORS[1]} -d 8 9 10
--- bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/99-autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/sh
-
-# ibus-daemon -dr &
-
-discord --start-minimized &
-syncthing -no-browser & # syncthing-gtk -m &
-wire-desktop --startup &
-# KEYBASE_START_UI=hideWindow run_keybase &
