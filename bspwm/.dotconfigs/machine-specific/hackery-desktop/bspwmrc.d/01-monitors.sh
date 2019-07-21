4e972d6882d449d76e09a5b964647842b6a36dd0
half cambodian hacker man <half-kh-hacker@hackery.site>
2019-07-21 02:30:58 +0100

    [bspwm] Allow bspwm configs to be used without xfce
    Also, on hackery-desktop, we can just hardcode the monitor names as we
    always have, since bspc sometimes gets the monitors the wrong way
    around?

--- bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
+++ bspwm/.dotconfigs/machine-specific/hackery-desktop/bspwmrc.d/01-monitors.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/sh
 
-MONITORS=( $(bspc query -M --names) )
-
-bspc monitor ${MONITORS[2]} -d 1 2 3 4 5 6 7
-bspc monitor ${MONITORS[1]} -d 8 9 10
+bspc monitor DisplayPort-1 -d 1 2 3 4 5 6 7
+bspc monitor HDMI-A-0 -d 8 9 10
