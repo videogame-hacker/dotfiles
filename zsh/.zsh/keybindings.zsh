64c6af4783bf7405b8740733edc81bcbe5a40b4e
half cambodian hacker man <half-kh-hacker@hackery.site>
2019-10-21 22:43:10 +0100

    [sway] Get rid of Kanshi
    Too many issues right now to use it
    
    Oh, and I also fixed ctrl-backspace in cool-retro-term with a very
    stupid hack.

--- zsh/.zsh/keybindings.zsh
+++ zsh/.zsh/keybindings.zsh
@@ -16,7 +16,9 @@ bindkey "${terminfo[kend]}"  end-of-line
 
 bindkey '^[[1;5C' forward-word
 bindkey '^[[1;5D' backward-word
-bindkey "${terminfo[kdch1]}" delete-char
+bindkey '^[^[[D' backward-word
+bindkey '^[^[[C' forward-word
+
 bindkey "${terminfo[kcbt]}" reverse-menu-complete
 bindkey "${terminfo[kpp]}" up-line-or-history
 bindkey "${terminfo[knp]}" down-line-or-history
@@ -29,17 +31,25 @@ autoload -U down-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
 
-bindkey "$terminfo[cuu1]" up-line-or-beginning-search
-bindkey "$terminfo[cud1]" down-line-or-beginning-search
-
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
+bindkey "${terminfo[cuu1]}" up-line-or-beginning-search
+bindkey "${terminfo[cud1]}" down-line-or-beginning-search
 
 bindkey '^[[5D' beginning-of-line
 bindkey '^[[5C' end-of-line
 
-bindkey '^[[3~' delete-char
 bindkey '^?' backward-delete-char
+if [[ "${terminfo[kdch1]}" != "" ]]; then
+  bindkey "${terminfo[kdch1]}" delete-char
+else
+  bindkey "^[[3~" delete-char
+  bindkey "^[3;5~" delete-char
+  bindkey "\e[3~" delete-char
+fi
 
-bindkey '^[[3;5~' delete-word
-bindkey '^H' backward-delete-word
+if [[ "$TERM" == "xterm-256color" ]]; then
+  bindkey '^H' backward-kill-word
+else
+  bindkey '^?' backward-kill-word
+fi
+
+bindkey '^[[3;5~' kill-word
