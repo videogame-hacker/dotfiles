5aa5b245ea8d672e465a73dd9a03a888c4aebfe1
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-03-10 19:00:40 +0000

    [bspwm] Remove 'night' from wallpaper tags and use featured
    For some reason, we're not getting many flowers in our flower-themed
    wallpapers

--- bspwm/.config/bspwm/bspwmrc.d/03-wallpaper.sh
+++ bspwm/.config/bspwm/bspwmrc.d/03-wallpaper.sh
@@ -1,8 +1,8 @@
 function fetch_wallpaper() {
-    TAGS="flower,night,pink"
+    TAGS="flower,pink"
 
     [ -d "/tmp/wallpaper-$USER" ] || mkdir "/tmp/wallpaper-$USER"
-    curl -L "https://source.unsplash.com/random/1920x1080?$TAGS" -o "/tmp/wallpaper-$USER/wallpaper.jpg"
+    curl -L "https://source.unsplash.com/featured/1920x1080?$TAGS" -o "/tmp/wallpaper-$USER/wallpaper.jpg"
 }
 
 if [ ! -f "/tmp/wallpaper-$USER/wallpaper.jpg" ]; then
