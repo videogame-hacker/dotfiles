dfd36d587c048050a1b31c0da3947708d3840040
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-03-10 19:02:36 +0000

    [bspwm] Use .cache/ instead of /tmp/ for wallpaper
    This means that when we reboot, we won't lose the wallpaper
    that we fetched in advance, since /tmp/ is usually a tmpfs
    that gets wiped on shutdown.

--- bspwm/.config/bspwm/bspwmrc.d/03-wallpaper.sh
+++ bspwm/.config/bspwm/bspwmrc.d/03-wallpaper.sh
@@ -1,13 +1,15 @@
+WALLPAPER_LOCATION="$HOME/.cache/.wallpaper.jpg"
+
 function fetch_wallpaper() {
     TAGS="flower,pink"
 
-    [ -d "/tmp/wallpaper-$USER" ] || mkdir "/tmp/wallpaper-$USER"
-    curl -L "https://source.unsplash.com/featured/1920x1080?$TAGS" -o "/tmp/wallpaper-$USER/wallpaper.jpg"
+    curl -L "https://source.unsplash.com/featured/1920x1080?$TAGS" -o "$WALLPAPER_LOCATION.dl"
+    mv "$WALLPAPER_LOCATION.dl" "$WALLPAPER_LOCATION"
 }
 
-if [ ! -f "/tmp/wallpaper-$USER/wallpaper.jpg" ]; then
+if [ ! -f "$WALLPAPER_LOCATION" ]; then
     fetch_wallpaper
 fi
 
-feh --bg-fill "/tmp/wallpaper-$USER/wallpaper.jpg" &
+feh --bg-fill "$WALLPAPER_LOCATION" &
 fetch_wallpaper &
