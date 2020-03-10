7695196f9d82c47baaa6d2a3c66ca87fa77844ea
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-03-10 13:38:28 +0000

    [bspwm] Change wallpaper theme to 'flower, night'

--- bspwm/.config/bspwm/bspwmrc.d/03-wallpaper.sh
+++ bspwm/.config/bspwm/bspwmrc.d/03-wallpaper.sh
@@ -1,5 +1,5 @@
 function fetch_wallpaper() {
-    TAGS="sakura,japan,night"
+    TAGS="flower,night"
 
     [ -d "/tmp/wallpaper-$USER" ] || mkdir "/tmp/wallpaper-$USER"
     curl -L "https://source.unsplash.com/random/1920x1080?$TAGS" -o "/tmp/wallpaper-$USER/wallpaper.jpg"
