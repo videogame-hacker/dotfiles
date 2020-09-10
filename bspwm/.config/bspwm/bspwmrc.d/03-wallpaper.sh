88d984f5ad7362753ef15e9e252b29d356dd08e5
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-09-10 20:03:46 +0100

    [bspwm] Add 'leaves' to wallpaper tags
    This means we get more plants and fewer pink, arty pictures.

--- bspwm/.config/bspwm/bspwmrc.d/03-wallpaper.sh
+++ bspwm/.config/bspwm/bspwmrc.d/03-wallpaper.sh
@@ -1,7 +1,7 @@
 WALLPAPER_LOCATION="$HOME/.cache/.wallpaper.jpg"
 
 function fetch_wallpaper() {
-    TAGS="flower,pink"
+    TAGS="flower,pink,leaves"
 
     curl -L "https://source.unsplash.com/random/1920x1080?$TAGS" -o "$WALLPAPER_LOCATION.dl"
     mv "$WALLPAPER_LOCATION.dl" "$WALLPAPER_LOCATION"
