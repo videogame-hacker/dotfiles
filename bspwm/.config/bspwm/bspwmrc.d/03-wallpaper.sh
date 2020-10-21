00cd9e972f87d4023eb52c09eb8ee8b708c60997
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-10-21 12:20:21 +0100

    [bspwm] Fetch higher resolution wallpapers

--- bspwm/.config/bspwm/bspwmrc.d/03-wallpaper.sh
+++ bspwm/.config/bspwm/bspwmrc.d/03-wallpaper.sh
@@ -3,7 +3,7 @@ WALLPAPER_LOCATION="$HOME/.cache/.wallpaper.jpg"
 function fetch_wallpaper() {
     TAGS="flower,pink,leaves"
 
-    curl -L "https://source.unsplash.com/random/1920x1080?$TAGS" -o "$WALLPAPER_LOCATION.dl"
+    curl -L "https://source.unsplash.com/random/2560x1440?$TAGS" -o "$WALLPAPER_LOCATION.dl"
     mv "$WALLPAPER_LOCATION.dl" "$WALLPAPER_LOCATION"
 }
 
