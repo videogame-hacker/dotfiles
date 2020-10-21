91a2018d3e729ae9fe60081d9af17ec24878cdfd
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-10-21 12:18:45 +0100

    [bspwm] Update monitor configuration for hackery-desktop

--- bspwm/.config/bspwm/bspwmrc.d/machine-specific/hackery-desktop/01-monitors.sh
+++ bspwm/.config/bspwm/bspwmrc.d/machine-specific/hackery-desktop/01-monitors.sh
@@ -1,4 +1,7 @@
-xrandr --output HDMI-0 --mode 1920x1080 --rate 74.97 --output DP-0 --mode 1920x1080 --rate 144.00 --right-of HDMI-0
+xrandr --output HDMI-0 --mode 1920x1080 --rotate right \
+    --output DP-2 --primary --mode 2560x1440 --rate 144.00 --right-of HDMI-0 \
+    --output DP-0 --mode 1920x1080 --rate 144.00 --right-of DP-2
 
-bspc monitor DP-0 -d 1 2 3 4 5 6 7
-bspc monitor HDMI-0 -d 8 9 10
+bspc monitor DP-2 -d 1 2 3 4 5 6 7
+bspc monitor DP-0 -d 8 9 10
+bspc monitor HDMI-0 -d 11 # It goes ALL the way up to eleven
